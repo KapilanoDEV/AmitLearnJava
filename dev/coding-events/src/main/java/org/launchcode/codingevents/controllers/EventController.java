55469ee3ba98150924fa7628791607c53b67baef
Amit Kapila <amitkapila@hotmail.com>
2023-08-20 15:09:22 +0100

    Prior to 14.4.1 Model-Binding class

--- dev/coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ dev/coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -10,17 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Controller
-@RequestMapping("events")
+@RequestMapping("events/")
 public class EventController {
 
 
-    // lives at /events/display
+    // lives at /events/
     @GetMapping
     public String displayAllEvents(Model model) {
-
 
         model.addAttribute("eventList",EventData.getAll());
         return "events/index";
@@ -34,8 +34,29 @@ public class EventController {
 
     // lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName,@RequestParam String eventDescription) {
+    public String processCreateEventsForm(@RequestParam String eventName,@RequestParam String eventDescription) {
         EventData.add(new Event(eventName,eventDescription));
-        return "redirect:/";
+        return "redirect:";
+    }
+    
+    // handler method that just displays the form
+    // Model passes data into the template
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+    	model.addAttribute("title", "Delete Events");
+    	model.addAttribute("events", EventData.getAll());
+    	return "events/delete";
+    }
+    
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+    	
+    	if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+    	
+    	return "redirect:";
     }
 }
